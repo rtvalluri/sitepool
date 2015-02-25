f310e758122b53023a9e6d55040ef7881be7078c
gitvrt <valluriraviteja@gmail.com>
2015-02-25 18:06:39 +0530

    Description about the  Sitepool

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
 console.log('This would be the main JS file.');
+$(function(){
+	$("#info-btn").click(function(){
+		$("#about-sitepool").addClass("flow");
+		console.log('This would be the main JS file.');
+	});
+	$("#hide").click(function(){
+		$("#about-sitepool").removeClass("flow");
+		console.log('This would be the main JS file.');
+	});
+});
+
