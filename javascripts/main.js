79581e9c5baf6d223681ca9849861d4ce9e06cc3
gitvrt <valluriraviteja@gmail.com>
2015-03-13 13:28:23 +0530

    Modified Sitepool description effects

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,12 +1,9 @@
-console.log('This would be the main JS file.');
-$(function(){
-	$("#info-btn").click(function(){
-		$("#about-sitepool").addClass("flow");
-		console.log('This would be the main JS file.');
-	});
-	$("#hide").click(function(){
-		$("#about-sitepool").removeClass("flow");
-		console.log('This would be the main JS file.');
-	});
+$(document).ready(function(){
+	$("#about-sitepool").hide();
+    $("#info-btn").click(function(){
+		$("#about-sitepool").show('slow');
+    });
+    $("#hide").click(function(){
+        $("#about-sitepool").hide('slow');
+    });
 });
-
