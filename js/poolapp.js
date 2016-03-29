ee219775c0b11935257da92a9164d3a53e8ae109
Raviteja Valluri <rtvalluri@users.noreply.github.com>
2016-03-29 12:32:26 +0530

    scroll code removed

--- js/poolapp.js
+++ js/poolapp.js
@@ -1,44 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    window.smoothScrollTo = function() {
-        var timer, start, factor;
-        return function(target, duration) {
-            var offset = window.pageYOffset, delta = target - window.pageYOffset;
-            // Y-offset difference
-            duration = duration || 700;
-            // default 1 sec animation
-            start = Date.now();
-            // get start time
-            factor = 0;
-            if (timer) {
-                clearInterval(timer);
-            }
-            function step() {
-                var y;
-                factor = (Date.now() - start) / duration;
-                // get interpolation factor
-                if (factor >= 1) {
-                    clearInterval(timer);
-                    // stop animation
-                    factor = 1;
-                }
-                y = factor * delta + offset;
-                window.scrollBy(0, y - window.pageYOffset);
-            }
-            timer = setInterval(step, 10);
-            return timer;
-        };
-    }();
-    var title = document.getElementById("title");
-    title.addEventListener("click", function() {
-        smoothScrollTo(0, 0);
-    }, false);
-    window.onload = function() {
-        smoothScrollTo(0, 0);
-    };
-});//end of ready function
-
 var app = angular.module("poolApp", []);
-
 app.controller("MainController", function($scope) {
     $scope.poolbase = [ {
         title: "Google",
